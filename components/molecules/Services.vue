b423bcd88d138c9a6542e21771d484d9281bc50c
Sabrina <sabrinath1@hotmail.com>
2023-08-16 14:26:59 -0300

    all services

---
+++ components/molecules/Services.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 gap-3 justify-center w-full h-auto mx-auto"
+  >
+    <div>
+      <AtomsCardServices
+        title="TELECOMUNICAÇÃO"
+        color="white"
+        content="Oferecemos serviços especializados que abrangem tanto a otimização de planos móveis, fixos e avançado empresariais quanto a assessoria jurídica em telecomunicações e consultoria empresarial na área de Telecom."
+      />
+    </div>
+    <div>
+      <AtomsCardServices
+        title="IDENTIDADE VISUAL"
+        color="white-green"
+        content="Em resumo, a identidade visual é mais do que apenas um logotipo bonito - é uma ferramenta estratégica que contribui para a percepção da marca, o reconhecimento e a construção de relacionamentos sólidos com os públicos-alvo. Ela desempenha um papel crucial na criação de uma imagem distintiva e memorável no mercado."
+      />
+    </div>
+    <div>
+      <AtomsCardServices
+        title="SITE"
+        color="white"
+        content="Ter um site é fundamental para uma empresa nos tempos modernos, independentemente do tamanho ou setor de atuação. O site atua como um ponto central de presença online da empresa e desempenha um papel essencial em várias áreas estratégicas"
+      />
+    </div>
+    <div>
+      <AtomsCardServices
+        title="LANDING PAGE"
+        color="white-green"
+        content="É uma página web bonita e funcional projetada para maximizar e otimizar a conversão de visitantes em clientes
+"
+      />
+    </div>
+  </div>
+</template>
--- components/organisms/Solutions.vue
+++ components/organisms/Solutions.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="flex w-full h-96"></div>
+  <div class="flex w-4/5 xl:w-3/5 justify-center items-center mx-auto"></div>
 </template>
